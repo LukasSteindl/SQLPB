9df6a1e0c0a8d2ee0ca4d1ae750762197ab867ab
STEINDL Lukas <SFP7W3B@post.at>
2018-07-26 12:43:20 +0200

    added theme to r plot

--- SQLBatchExecution-DurationHistogram.r
+++ SQLBatchExecution-DurationHistogram.r
@@ -5,4 +5,5 @@ ggplot(dataset)+
 geom_histogram(binwidth=1000,aes(x = duration_in_microseconds/1000, y =..count../sum(..count..))) +
 scale_y_continuous(labels = percent_format()) +
 ylab("Percent of Total Execution Number") +
-ggtitle("SQLBatchExecution-DurationHistogram")
+ggtitle("SQLBatchExecution-DurationHistogram")+
+theme_light()
